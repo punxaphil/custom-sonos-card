9b1edc3eecc093bcbb41af336d2cfb2f3ac09bee
Johan Frick <johan.frick@gmail.com>
2023-09-16 20:47:00 +0200

    fix bugs where changes where not saved

--- src/editor/predefined-group-editor.ts
+++ src/editor/predefined-group-editor.ts
@@ -1,57 +1,44 @@
 import { html, TemplateResult } from 'lit';
-import { property } from 'lit/decorators.js';
+import { property, state } from 'lit/decorators.js';
 import { mdiCheck, mdiDelete } from '@mdi/js';
 import { BaseEditor } from './base-editor';
-import { ConfigPredefinedGroup, ConfigPredefinedGroupPlayer, PredefinedGroup } from '../types';
+import { ConfigPredefinedGroupPlayer, PredefinedGroup } from '../types';
 
 class PredefinedGroupEditor extends BaseEditor {
   @property() index!: number;
-  private predefinedGroup!: PredefinedGroup;
+  @state() predefinedGroup!: PredefinedGroup<ConfigPredefinedGroupPlayer>;
+
+  private schema = [
+    { type: 'string', name: 'name', required: true },
+    { type: 'string', name: 'media' },
+    {
+      name: 'entities',
+      selector: { entity: { multiple: true, filter: { integration: 'sonos', domain: 'media_player' } } },
+    },
+  ];
 
   protected render(): TemplateResult {
     this.config = this.store.config;
     this.hass = this.store.hass;
-
-    this.predefinedGroup = this.store.predefinedGroups?.[this.index || 0] || {
-      name: '',
-      media: '',
-      entities: [],
-    };
-    const predefinedGroupWithoutVolumes: PredefinedGroup<string> = {
-      ...this.predefinedGroup,
-      entities: this.predefinedGroup.entities.map((pgItem) => pgItem.player.id),
-    };
-    const schema = [
-      {
-        type: 'string',
-        name: 'name',
-        required: true,
-      },
-      {
-        type: 'string',
-        name: 'media',
-      },
-      {
-        name: 'entities',
-        selector: { entity: { multiple: true, filter: { integration: 'sonos', domain: 'media_player' } } },
-      },
-    ];
+    if (!this.predefinedGroup) {
+      this.initPredefinedGroup();
+    }
     return html`
       <h2>Add/Edit Predefined Group</h2>
       <sonos-card-editor-form
-        .data=${predefinedGroupWithoutVolumes}
-        .schema=${schema}
+        .data=${this.getPredefinedGroupWithoutVolumes()}
+        .schema=${this.schema}
         .store=${this.store}
-        .changed=${(ev: CustomEvent) => this.groupChanged(ev, this.index)}
+        .changed=${(ev: CustomEvent) => this.groupChanged(ev)}
       ></sonos-card-editor-form>
       <div>
         <h3>Volumes - will be set when players are grouped</h3>
-        ${this.predefinedGroup.entities.map(({ player: { id, name }, volume }) => {
+        ${this.predefinedGroup.entities.map(({ player, volume }) => {
           const schema = [
             {
               type: 'integer',
               name: 'volume',
-              label: `${name}${volume ? `: ${volume}` : ''}`,
+              label: `${this.hass.states[player].attributes.friendly_name}${volume !== undefined ? `: ${volume}` : ''}`,
               valueMin: 0,
               valueMax: 100,
             },
@@ -61,65 +48,83 @@ class PredefinedGroupEditor extends BaseEditor {
               .data=${{ volume }}
               .schema=${schema}
               .store=${this.store}
-              .changed=${(ev: CustomEvent) => this.volumeChanged(ev, this.index, id)}
+              .changed=${(ev: CustomEvent) => this.volumeChanged(ev, player)}
             ></sonos-card-editor-form>
           `;
         })}
       </div>
       <ha-control-button-group>
-        <ha-control-button @click="${this.dispatchClose}">
+        <ha-control-button @click="${this.savePredefinedGroup}">
           OK<ha-svg-icon .path=${mdiCheck} label="OK"></ha-svg-icon>
         </ha-control-button>
-        ${this.predefinedGroup.name
-          ? html`<ha-control-button
-              @click="${() => {
-                this.config.predefinedGroups = this.config.predefinedGroups?.filter((_, index) => index !== this.index);
-                this.index = -1;
-                this.configChanged();
-                this.dispatchClose();
-              }}"
-            >
-              Delete<ha-svg-icon .path=${mdiDelete} label="Delete"></ha-svg-icon>
-            </ha-control-button>`
-          : ''}
+        <ha-control-button @click="${this.deletePredefinedGroup}">
+          Delete<ha-svg-icon .path=${mdiDelete} label="Delete"></ha-svg-icon>
+        </ha-control-button>
       </ha-control-button-group>
     `;
   }
 
-  private groupChanged(ev: CustomEvent, index: number): void {
+  private initPredefinedGroup() {
+    const configPg = this.config.predefinedGroups?.[this.index || 0];
+    if (configPg) {
+      const entities = configPg.entities.map((entity) => {
+        return typeof entity === 'string' ? { player: entity } : entity;
+      });
+      this.predefinedGroup = { ...configPg, entities };
+    } else {
+      this.predefinedGroup = { name: '', media: '', entities: [] };
+    }
+  }
+
+  private getPredefinedGroupWithoutVolumes() {
+    return {
+      ...this.predefinedGroup,
+      entities: this.predefinedGroup.entities.map((pgItem) => pgItem.player),
+    };
+  }
+
+  private groupChanged(ev: CustomEvent): void {
     const changed: PredefinedGroup<string> = ev.detail.value;
     const entities: ConfigPredefinedGroupPlayer[] = changed.entities.map((changedPlayerId) => {
-      const existing = this.predefinedGroup.entities.find(({ player: { id } }) => {
-        return id === changedPlayerId;
+      const existing = this.predefinedGroup.entities.find(({ player }) => {
+        return player === changedPlayerId;
       });
-      return existing ? { ...existing, player: existing.player.id } : { player: changedPlayerId };
+      return existing ?? { player: changedPlayerId };
     });
-    const configItem: ConfigPredefinedGroup = {
+    this.predefinedGroup = {
       ...changed,
       entities,
     };
+  }
+
+  private volumeChanged(ev: CustomEvent, playerId: string) {
+    const volume = ev.detail.value.volume;
+    const entities = this.predefinedGroup.entities.map((entity) =>
+      entity.player === playerId ? { ...entity, volume } : entity,
+    );
+    this.predefinedGroup = { ...this.predefinedGroup, entities };
+  }
+
+  private savePredefinedGroup() {
     let groups = this.config.predefinedGroups;
     if (!Array.isArray(groups)) {
       groups = [];
     }
-    if (groups[index]) {
-      groups[index] = configItem;
+    if (groups[this.index]) {
+      groups[this.index] = this.predefinedGroup;
     } else {
-      groups = [...groups, configItem];
+      groups = [...groups, this.predefinedGroup];
     }
     this.config.predefinedGroups = groups;
     this.configChanged();
+    this.dispatchClose();
   }
 
-  private volumeChanged(ev: CustomEvent, index: number, playerId: string) {
-    const group = this.config.predefinedGroups?.[index];
-    if (group) {
-      const volume = ev.detail.value.volume;
-      group.entities = group.entities
-        .map((entity) => (typeof entity === 'string' ? { player: entity } : entity))
-        .map((entity) => (entity.player === playerId ? { ...entity, volume } : entity));
-    }
+  private deletePredefinedGroup() {
+    this.config.predefinedGroups = this.config.predefinedGroups?.filter((_, index) => index !== this.index);
+    this.index = -1;
     this.configChanged();
+    this.dispatchClose();
   }
 }
 
