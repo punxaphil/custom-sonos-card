d0e24375c907521660984dcfa68efaa418992fca
Johan Frick <johan.frick@gmail.com>
2024-10-21 18:44:10 +0200

    shuffle and repeat

--- src/sections/queue.ts
+++ src/sections/queue.ts
@@ -7,6 +7,9 @@ import { until } from 'lit-html/directives/until.js';
 import { customEvent } from '../utils/utils';
 import { mdiPlaylistEdit, mdiTrashCanOutline } from '@mdi/js';
 import '../components/media-row';
+import { MediaPlayerEntityFeature } from '../types';
+
+const { SHUFFLE_SET, REPEAT_SET } = MediaPlayerEntityFeature;
 
 export class Queue extends LitElement {
   @property() store!: Store;
@@ -16,17 +19,24 @@ export class Queue extends LitElement {
   render() {
     this.activePlayer = this.store.activePlayer;
     const selected = this.activePlayer.attributes.queue_position - 1;
-
     return html`${until(
-      this.store.hassService.getQueue(this.store.activePlayer).then(
+      this.store.hassService.getQueue(this.activePlayer).then(
         (queue) => html`
           <div class="header">
-            <div class="title">${this.store.config.queueTitle ?? 'Play Queue'}</div>
-            <ha-icon-button
-              .path=${mdiPlaylistEdit}
-              @click=${this.toggleEditMode}
-              selected=${this.editMode || nothing}
-            ></ha-icon-button>
+            <div class="title">
+              ${this.store.config.queueTitle ??
+              (this.activePlayer.attributes.media_playlist
+                ? `Playlist: ${this.activePlayer.attributes.media_playlist}`
+                : `Play Queue`) + (this.activePlayer.attributes.media_channel ? ' (not active)' : '')}
+            </div>
+            <div class="header-icons">
+              <sonos-ha-player .store=${this.store} .features=${[SHUFFLE_SET, REPEAT_SET]}></sonos-ha-player>
+              <ha-icon-button
+                .path=${mdiPlaylistEdit}
+                @click=${this.toggleEditMode}
+                selected=${this.editMode || nothing}
+              ></ha-icon-button>
+            </div>
           </div>
           <div class="list">
             <mwc-list multi>
@@ -82,6 +92,9 @@ export class Queue extends LitElement {
           display: flex;
           justify-content: space-between;
           padding: 0.5rem;
+        }
+        .header-icons > * {
+          display: inline-block;
         }
         .title {
           text-align: center;
