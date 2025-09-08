punxaphil/custom-sonos-card

commit 6c0fcad3a1bae0d61332e1795d632bb0ee1426c8

Support entity_picture_local for Music Assistant integration (#647)

src/sections/player.ts
@@ -60,9 +60,12 @@ export class Player extends LitElement {
 
   private getArtworkImage() {
     const prefix = this.config.artworkHostname || '';
-    const { media_title, media_artist, media_album_name, media_content_id, media_channel, entity_picture } =
+    const { media_title, media_artist, media_album_name, media_content_id, media_channel, entity_picture, entity_picture_local, app_id } =
       this.activePlayer.attributes;
     let entityImage = entity_picture ? prefix + entity_picture : entity_picture;
+    if (app_id === 'music_assistant') {
+      entityImage = entity_picture_local ? prefix + entity_picture_local : entity_picture_local;
+    }
     let sizePercentage = undefined;
     const overrides = this.config.mediaArtworkOverrides;
     if (overrides) {

commit 6b2bb1c771fa7404ce50911a43858b1a52bf96f5

Fix entity_picture_local for Music Assistant integration
Fix regression introduced in PR #647 which introduced support for the entity_picture_local attribute used with local-only dashboards.

src/sections/player.ts
@@ -63,7 +63,7 @@ export class Player extends LitElement {
     const { media_title, media_artist, media_album_name, media_content_id, media_channel, entity_picture, entity_picture_local, app_id } =
       this.activePlayer.attributes;
     let entityImage = entity_picture ? prefix + entity_picture : entity_picture;
-    if (app_id === 'music_assistant') {
+    if (app_id === 'music_assistant' && entity_picture_local) {
       entityImage = entity_picture_local ? prefix + entity_picture_local : entity_picture_local;
     }
     let sizePercentage = undefined;