1b79e18ccef125a4956464806d64e8e0b3835588
Johan Frick <johan.frick@gmail.com>
2025-09-17 21:11:47 +0200

    fix: Switching Player needs two attempts
    fixes #663

--- src/model/store.ts
+++ src/model/store.ts
@@ -174,7 +174,7 @@ export default class Store {
   }
 
   private determineActivePlayer(activePlayerId?: string): MediaPlayer {
-    const playerId = activePlayerId || this.config.entityId || this.getActivePlayerFromUrl();
+    const playerId = activePlayerId || this.getActivePlayerFromUrl() || this.config.entityId;
     return (
       this.allGroups.find((group) => group.getMember(playerId) !== undefined) ||
       this.allGroups.find((group) => group.isPlaying()) ||
