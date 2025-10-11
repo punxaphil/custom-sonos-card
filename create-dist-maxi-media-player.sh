5f4f0f5ff2b2c9b84d61ee3830b50b0e1a2f4a1f
Johan Frick <johan.frick@gmail.com>
2025-10-11 08:12:28 +0200

    docs: make sure comparison is correct

--- create-dist-maxi-media-player.sh
+++ create-dist-maxi-media-player.sh
@@ -25,7 +25,7 @@ cp $FILE $DIR/
 cd $DIR || exit
 sed -i '/\/\/#ONLY_SONOS_CARD_START/,/\/\/#ONLY_SONOS_CARD_END/d' $FILE
 sed -i ':a;N;$!ba;s/[^\n]*#ONLY_SONOS_CARD[^\n]*\n//g' $FILE
-sed -i 's/custom-sonos-card/maxi-media-player/g' $FILE
-sed -i 's/custom:sonos-card/custom:maxi-media-player/g' $FILE
-sed -i 's/Sonos Card/Maxi Media Player/g' $FILE
-sed -i 's/sonos-/mxmp-/g' $FILE
+sed -i '/NO_REPLACE_ON_THIS_LINE/!s/custom-sonos-card/maxi-media-player/g' $FILE
+sed -i '/NO_REPLACE_ON_THIS_LINE/!s/custom:sonos-card/custom:maxi-media-player/g' $FILE
+sed -i '/NO_REPLACE_ON_THIS_LINE/!s/Sonos Card/Maxi Media Player/g' $FILE
+sed -i '/NO_REPLACE_ON_THIS_LINE/!s/sonos-/mxmp-/g' $FILE
