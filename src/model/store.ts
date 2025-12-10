f56e0c0c31e65ddc6d2ef0eb4dbd886598923a0c
Johan Frick <johan.frick@gmail.com>
2025-12-10 23:30:53 +0100

    fix: remove console warning for unavailable players

--- src/model/store.ts
+++ src/model/store.ts
@@ -154,9 +154,6 @@ export default class Store {
       const isGrouped = groupIds?.length > 1;
       const isMainInGroup = isGrouped && groupIds && groupIds[0] === hassEntity.entity_id;
       const available = this.hass.states[hassEntity.entity_id]?.state !== 'unavailable';
-      if (!available) {
-        console.warn(`Player ${hassEntity.entity_id} is unavailable`);
-      }
       return (!isGrouped || isMainInGroup) && available;
     } catch (e) {
       console.error('Failed to determine main player', JSON.stringify(hassEntity), e);
